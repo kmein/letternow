5e556970e35640375bdecb1eef83a4a47a11d4da
Kierán Meinhardt <kmein@posteo.de>
2026-05-22 15:53:04 +0200

    feat: add nix package output to flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "letternow dev environment";
+  description = "letternow web application";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -12,6 +12,24 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "letternow";
+          version = "1.0.0";
+
+          src = ./.;
+
+          npmDepsHash = "sha256-dpU3zHjbvKLL1b+o6QLGvxiDpAuN3GW+8gGC4K8Ydx0=";
+
+          buildInputs = [ pkgs.nodejs_20 ];
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/share/letternow
+            cp -r dist/* $out/share/letternow/
+            runHook postInstall
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nodejs_20
